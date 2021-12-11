82ed8f8fc1dc03ec537b5e3f298444c62ec39361
thole20042001 <nhattho03@gmail.com>
2021-12-11 14:54:24 +0700

    upgrade pagination

--- app/controllers/ProductsController.js
+++ app/controllers/ProductsController.js
@@ -1,77 +1,122 @@
-const { response } = require('express');
-const ProductsService = require('../services/ProductsService');
+const { models } = require('../models');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+
+const { getPagination } = require('../../public/js/pagination');
+const { getPagingData } = require('../../public/js/pagination');
+// const { models } = require('../models');
+const productsService = require('../services/ProductsService');
+
 
 class ProductController {
 
-    // // [POST] /products/filter/:slug
-    // filter(req, res, next) {
-    //     let page = +req.query.page || 1;
-    //     const perPage = 9;
-    //     let color = req.body.data;
+    // [GET] /products
+    list = async (req, res) => {
+        const { page, size, term } = req.query;
+        const { limit, offset } = getPagination(page, size);
 
-    //     const filterProd = ProductsService.filterByColor(color, !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
+        await productsService.listProduct(term, limit, offset)
+            .then((data) => {
+                const response = getPagingData(data, page, limit);
+                res.render('products/products', {
+                    products: response.items,
+                    totalPages: response.totalPages,
+                    currentPage: response.currentPage,
+                    totalItems: response.totalItems,
+                });
+            })
+            .catch(err => {
+                res.send(err);
+            })
+    }
 
-    //     filterProd.then(products => {
-    //         let view = {
-    //             products: products.rows,
-    //             layout: false,
-    //             raw: true,
-    //             pagination: {
-    //                 page: req.query.page || 1,
-    //                 pageCount: Math.ceil(products.count / perPage),
-    //             }
+    // // [GET] /product
+    // pagi = async (req, res) => {
+    //     let { term, color, price_start, price_end, page } = req.query;
+    //     if (page) {
+    //         page = parseInt(page);
+    //         if (page < 1) {
+    //             page = 1;
     //         }
-    //         res.render('products/searchProd', view);
-    //     })
-    //         .catch(next);
+    //         let skipAmount = (page - 1) * 9;
+    //         console.log("1----")
+    //         await productsService.pagin(term, color, price_start, price_end, 9, skipAmount)
+    //             .then((data) => {
+    //                 res.json(data);
+    //             })
+    //             .catch(err => {
+    //                 res.send(err);
+    //             })
+    //     } else {
+    //         console.log("2----")
+    //         await models.products.findAll()
+    //             .then((data) => {
+    //                 res.json(data);
+    //             })
+    //             .catch(err => {
+    //                 res.send(err);
+    //             })
+    //     }
     // }
 
     // [POST] /products/filter/:slug
-    filter(req, res, next) {
-        let page = +req.query.page || 1;
-        const perPage = 9;
-        let price_start = req.body.price_start;
-        let price_end = req.body.price_end;
-        let color = req.body.color;
-        const filterProd = ProductsService.filter(color, price_start, price_end, !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
+    filter = async (req, res, next) => {
+        const { page, size, color, price_start, price_end} = req.body;
+        const { limit, offset } = getPagination(page, size);
 
-        filterProd.then(products => {
-            let view = {
-                products: products.rows,
+        await productsService.filter(color, price_start, price_end, limit, offset)
+        .then((data) => {
+            const response  = getPagingData(data, page, limit);
+            res.render('products/searchProd', { 
                 layout: false,
-                raw: true,
-                pagination: {
-                    page: req.query.page || 1,
-                    pageCount: Math.ceil(products.count / perPage),
-                }
-            }
-            res.render('products/searchProd', view);
+                products: response.items,
+                totalPages: response.totalPages,
+                currentPage: response.currentPage,
+                totalItems: response.totalItems,
+            });
         })
-            .catch(next);
+        .catch(err => {
+            res.send(err);
+        })
+
+        //const filterProd = productsService.filter(color, price_start, price_end, !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
+
+        // filterProd.then(products => {
+        //     let view = {
+        //         products: products.rows,
+        //         layout: false,
+        //         raw: true,
+        //         // pagination: {
+        //         //     page: req.query.page || 1,
+        //         //     pageCount: Math.ceil(products.count / perPage),
+        //         // }
+        //     }
+        //     res.render('products/searchProd', view);
+        // })
+        //     .catch(next);
     }
 
-    // [GET] /products/search
-    search(req, res, next) {
-        let page = +req.query.page || 1;
-        const perPage = 9;
-        let { term = '' } = req.query;
+    // // [GET] /products/search
+    // search(req, res, next) {
+    //     let page = +req.query.page || 1;
+    //     const perPage = 9;
+    //     let { term = '' } = req.query;
 
-        term = term.toLowerCase();
-        const products = ProductsService.searchByName(term, !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
+    //     term = term.toLowerCase();
+    //     const products = productsService.searchByName(term, !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
 
-        console.log("my term: " + term);
-        products.then(products => {
-            res.render('products/products', {
-                products: products.rows,
-                pagination: {
-                    page: req.query.page || 1,
-                    pageCount: Math.ceil(products.count / perPage),
-                },
-                term: term,
-            })
-        })
-            .catch(next);
-    }
+    //     console.log("my term: " + term);
+    //     products.then(products => {
+    //         res.render('products/products', {
+    //             products: products.rows,
+    //             pagination: {
+    //                 page: req.query.page || 1,
+    //                 pageCount: Math.ceil(products.count / perPage),
+    //             },
+    //         })
+    //     })
+    //         .catch(next);
+    // }
 
     // // [POST] /products/search (AJAX)
     // search (req, res, next) {
@@ -101,10 +146,10 @@ class ProductController {
     // [GET] /products/:productid
     show = async (req, res, next) => {
         let page = +req.query.page || 1;
-        const productDetail = ProductsService.show(req.params.productid);
+        const productDetail = productsService.show(req.params.productid);
 
         // const {brand} = productDetail;
-        const relatedProd = await ProductsService.index(0, 8);
+        const relatedProd = await productsService.index(0, 8);
         // const relatedProducts = ProductService.index(0);
 
         productDetail.then(product => {
@@ -116,29 +161,19 @@ class ProductController {
         ).catch(next);
     }
 
-    // [GET] /products
-    index = async (req, res) => {
-        const products = await ProductsService.index(!isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
-        res.render('products/products', {
-            products,
-            pagination: {
-                page: req.query.page || 1,
-                pageCount: 9
-            }
-        });
-    }
-
-    // [GET] /products/filter=string
-    // filter = async (req, res) => {
-    //     const products = await ProductService.filter(req.query.filter);
+    // // [GET] /products
+    // index = async (req, res) => {
+    //     const products = await productsService.index(!isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0);
     //     res.render('products/products', {
     //         products,
     //         pagination: {
     //             page: req.query.page || 1,
-    //             pageCount: perPage
+    //             pageCount: 9
     //         }
     //     });
     // }
+
+
 }
 
 module.exports = new ProductController;
--- app/routes/products.js
+++ app/routes/products.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const productController = require('../controllers/ProductsController');
 
+//router.get('/', productController.pagi);
+
 router.post('/filter', productController.filter);
 
-// router.post('/search', productController.search);
-router.get('/search', productController.search);
+// // router.post('/search', productController.search);
+// router.get('/search', productController.search);
 
 router.get('/:productid', productController.show);
 
-router.get('/', productController.index);
+// router.get('/', productController.index);
+router.get('/', productController.list);
 
 module.exports = router;
