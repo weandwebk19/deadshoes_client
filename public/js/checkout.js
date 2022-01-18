0a2360beb9287998d8b7942735d8a65bbe9dac98
thole20042001 <nhattho03@gmail.com>
2022-01-18 21:34:03 +0700

    update checkout validation

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -2,8 +2,17 @@ $(document).ready(function () {
     const checkoutForm = document.forms['checkoutForm'];
 
     $('#btn-proceed-order').click(function (e) {
-        e.preventDefault();
-        checkoutForm.action = '/checkout/' + $(this).attr('order') + '?_method=DELETE';
-        checkoutForm.submit();
+        const name = $('#name').val();
+        const email = $('#email').val();
+        const phone = $('#phone').val();
+        const address = $('#address').val();
+
+        if (!name || !email || !phone || !address) {
+            alert("Please fill out the receiver's informations");
+        } else {
+            e.preventDefault();
+            checkoutForm.action = '/checkout/' + $(this).attr('order') + '?_method=DELETE';
+            checkoutForm.submit();
+        }
     })
 })
